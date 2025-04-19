4b2b88c596a58eef4a890bcbecdceb1f954f320c
Irshath Nagouri <irsnag@gmail.com>
2025-04-19 18:32:32 +0200

    update dataset creation

--- create_dataset.m
+++ create_dataset.m
@@ -15,9 +15,53 @@ simParameters.NumUEs          = 15; % Number of UE positions; applicable if ueDr
 simParameters.UESeparation    = [30 30]; % UE separation in x and y axes in meters; applicable if ueDropType is 'grid'
 simParameters.UEAntennaHeight = 1.5; % UE antenna height on the z axis in meters
 
-% Set a random number seed for repeatability
-simParameters.Seed = 2000;
-rng(simParameters.Seed,"twister");
+% Number of different seeds
+numSeeds = 3;
 
-% Configure and visualize the scenario
-simParameters = h5GIndoorFactoryScenario(simParameters);
+% Initialize arrays to hold all the data
+allTrainInputs = [];
+allTrainLabels = [];
+
+allValInputs = [];
+allValLabels = [];
+
+for i = 1:numSeeds
+    fprintf("Processing seed %d...\n", i);
+
+    % Set new seed and regenerate scenario
+    simParameters.Seed = i;
+    rng(simParameters.Seed, "twister");
+    
+    simParameters = h5GIndoorFactoryScenario(simParameters);
+    channels = h38901ChannelSetup(simParameters);
+    
+    % Generate training and validation datastores
+    [dsTrain, dsValidate, ~, ~] = channels2ImageDataset(simParameters, channels);
+    
+    % --- Accumulate TRAIN data
+    reset(dsTrain);
+    while hasdata(dsTrain)
+        data = read(dsTrain);
+        allTrainInputs = cat(4, allTrainInputs, data{1});   % [32 x 32 x 18 x N]
+        allTrainLabels = [allTrainLabels; data{2}];         % [N x 3]
+    end
+
+    % --- Accumulate VALIDATION data
+    reset(dsValidate);
+    while hasdata(dsValidate)
+        data = read(dsValidate);
+        allValInputs = cat(4, allValInputs, data{1});
+        allValLabels = [allValLabels; data{2}];
+    end
+end
+
+% Create ArrayDatastore for inputs and labels
+adsTrainX = arrayDatastore(allTrainInputs, 'IterationDimension', 4);
+adsTrainY = arrayDatastore(allTrainLabels);
+
+adsValX = arrayDatastore(allValInputs, 'IterationDimension', 4);
+adsValY = arrayDatastore(allValLabels);
+
+% Combine into one paired datastore
+dataTrain = combine(adsTrainX, adsTrainY);
+dataValidate = combine(adsValX, adsValY);
